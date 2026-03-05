4c983c02450c8689afdba19fd7de1cf661074d8e
glekoliveira <linux.guilherme@gmail.com>
2026-03-05 10:31:23 -0300

    fix gunicorn module path

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Pipeline funcionando no Cloud Run 🚀"
+    return "CI/CD funcionando 🚀"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
